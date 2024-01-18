c3100b2c0a05ea04e0667a6a4f5c87fb86671266
Vignesh Vijayakumar <94455180+Vignesh27V@users.noreply.github.com>
2024-01-18 14:37:30 +0530

    Update staticex.c

--- staticex.c
+++ staticex.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 void func(){
-	int NonStaticVar = 0;//normal declaration var address are store in .data section
+	int NonStaticVar = 0;//normal declaration var address are store in .heap section
 	//re-declare every time blocks call
-	static int StaticVar = 0;//where satic and global var address are store in .bss section
+	static int StaticVar = 0;//where satic and global var address are store in .bss or .data section
 	//static will declare once in one execution irrespective of block life cycle
 	NonStaticVar +=1;
 	StaticVar +=1;
